f823a3a2135b3c7084d8992b36afe160b450a726
Nithya Sivan <100074248+123nithya@users.noreply.github.com>
2024-06-29 11:49:54 +0530

    Update Home.jsx

--- coffeelandingpage/src/Components/Home.jsx
+++ coffeelandingpage/src/Components/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import NavBar from "./NavBar";
 import HeroSection from "./HeroSection";
+import BottomLine from "./BottomLine";
+import ReadMore from "./ReadMore";
+import DidYouKnow from "./DidYouKnow";
+import Coffee from "./Coffee";
+import Order from "./Order";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <HeroSection />
+      <BottomLine />
+      <ReadMore />
+      <DidYouKnow />
+      <Coffee />
+      <Order />
+      <Footer />
     </div>
   );
 };
