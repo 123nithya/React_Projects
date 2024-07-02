5382f3ea7e10660c0ac387c8fe69b10c10d19922
Nithya Sivan <100074248+123nithya@users.noreply.github.com>
2024-07-02 21:38:14 +0530

    Add files via upload

---
+++ animations-in-react_gsap/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import AnimationGsapReact from "./AnimationGsapReact";
+
+function App() {
+  return (
+    <div className="App">
+      <AnimationGsapReact />
+    </div>
+  );
+}
+
+export default App;
